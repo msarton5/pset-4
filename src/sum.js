5710e30a097858ab8e481a2af7e71e72ec5c380e
Molly Sarton <MSarton@ucvts.tec.nj.us>
2019-11-20 13:17:24 -0500

    edited sum again

--- src/sum.js
+++ src/sum.js
@@ -3,22 +3,24 @@ const readlineSync = require("readline-sync");
 MIN = Number.MIN_SAFE_INTEGER
 MAX = Number.MAX_SAFE_INTEGER
 
-let Lower = readlineSync.question("\nLower bound: ")
-let Upper = readlineSync.question("Upper bound: ")
+let Lower = Number(readlineSync.question("\nLower bound: "));
+let Upper = Number(readlineSync.question("Upper bound: "));
 
 let Answer = 0
 
 while (Lower > Upper) {
-    Lower = readlineSync.question("Lower bound: ")
-    Upper = readlineSync.question("Upper bound: ")
-} if (Lower % 2 == 0) {
-    for(a = Lower ; a <= Upper ; a += 2) {
-        Answer = a + Answer
-    }
-} else {
-    for(a = Lower + 1 ; a <= Upper ; a += 2) {
-        Answer = a + Answer
-    }
+    Lower = Number(readlineSync.question("Lower bound: "));
+    Upper = Number(readlineSync.question("Upper bound: "));
 }
 
-console.log("\n" + Answer + ".\n")
+if (Lower % 2 == 0) {
+    for (let a = Lower ; a <= Upper ; a += 2) {
+        Answer = a + Answer
+    }
+    console.log("\n" + Answer + ".\n")
+} else {
+    for(let a = Lower + 1 ; a <= Upper ; a += 2) {
+        Answer = a + Answer
+    }
+    console.log("\n" + Answer + ".\n")
+}
