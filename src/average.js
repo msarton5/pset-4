4ac9145d813ac3a3b3d1b8f1a1c6f2b51a764c7e
msarton5 <55455852+msarton5@users.noreply.github.com>
2019-11-24 20:38:15 -0500

    Update average.js

--- src/average.js
+++ src/average.js
@@ -8,17 +8,16 @@ let count = 0
 MIN = 0
 MAX = Number.MAX_SAFE_INTEGER
 
-let nni = 1
+nni = 1
 
 while (nni >= 0 || count <= 1) {
     nni = Number(readlineSync.question("Non-negative integer: "));
-    add = add + nni
     count = count + 1
-  if (nni > 0 && nni <= Number.MAX_SAFE_INTEGER) {
+  if (nni >= 0 && nni <= Number.MAX_SAFE_INTEGER) {
       add = add + nni;
   } else if (count > 1 && nni < 0) {
-      let answer = (add) / (count)
-      console.log("\n" + Intl.NumberFormat("en", { style: 'decimal', maximumFractionDigits:3, minimumFractionDigits:3 }).format(answer) + ".")
+      let answer = (add) / (count - 1)
+      console.log("\n" + Intl.NumberFormat("en", { style: 'decimal', maximumFractionDigits:3, minimumFractionDigits:3 }).format(answer) + ".\n")
   } else {
     --count;
   }
