c06a9118228f1bb67a535773c7caab2f50716ec3
Molly Sarton <MSarton@ucvts.tec.nj.us>
2019-11-25 11:58:12 -0500

    hsgfhjsfcja

--- src/factors.js
+++ src/factors.js
@@ -1,26 +1,26 @@
 const readlineSync = require("readline-sync");
 
-console.log()
+console.log();
 
-MIN = 1
-MAX = Number.MAX_SAFE_INTEGER
+MIN = 1;
+MAX = Number.MAX_SAFE_INTEGER;
 
-nni = -1
-string = 0
+nni = -1;
+output = "";
 
 while (nni < 1 || nni > Number.MAX_SAFE_INTEGER) {
       nni = Number(readlineSync.question("Positive integer: "));
 }
 
-for (i = 0; i <= Math.sqrt(nni); i++) {
-  if ((nni / i % 1 == 0)) {
-    string = String(i) + "," + String(nni/i)
-    if (i < Math.sqrt(nni)) {
-      string = String(i) + ","
-    } else {
-       string = string + "."
+for (let i = 0; i <= Math.sqrt(nni); i++) {
+    if (nni % i == 0) {
+      output += i + ", ";
+      if (i != (nni / i)) {
+        output += (nni / i) + ", ";
+      }
+
     }
-  }
 }
 
-console.log(string)
+output = output.substring(0, output.length - 2) + ".";
+console.log(output);
--- src/mario.js
+++ src/mario.js
@@ -5,60 +5,18 @@ console.log()
 MIN = 1
 MAX = 24
 
-height = 1
+height = 0;
 
-while (height <= 1 || height >= 24 || Number.isNaN(height)) {
-    height = Number(readlineSync.question("Height: "));
+while (height < 1 || height > 24 || Number.isNaN(height)) {
+  height = Number(readlineSync.question("Height: "));
+}
 
-  if (height == 1) {
-    console.log("\n##")
-  } else if (height == 2) {
-    console.log("\n###")
-  } else if (height == 3) {
-    console.log("\n####")
-  } else if (height == 4) {
-    console.log("\n#####")
-  } else if (height == 5) {
-    console.log("\n######")
-  } else if (height == 6) {
-    console.log("\n#######")
-  } else if (height == 7) {
-    console.log("\n########")
-  } else if (height == 8) {
-    console.log("\n#########")
-  } else if (height == 9) {
-    console.log("\n##########")
-  } else if (height == 10) {
-    console.log("\n###########")
-  } else if (height == 11) {
-    console.log("\n############")
-  } else if (height == 12) {
-    console.log("\n#############")
-  } else if (height == 13) {
-    console.log("\n##############")
-  } else if (height == 14) {
-    console.log("\n###############")
-  } else if (height == 15) {
-    console.log("\n################")
-  } else if (height == 16) {
-    console.log("\n#################")
-  } else if (height == 17) {
-    console.log("\n##################")
-  } else if (height == 18) {
-    console.log("\n###################")
-  } else if (height == 19) {
-    console.log("\n####################")
-  } else if (height == 20) {
-    console.log("\n#####################")
-  } else if (height == 21) {
-    console.log("\n######################")
-  } else if (height == 22) {
-    console.log("\n#######################")
-  } else if (height == 23) {
-    console.log("\n########################")
-  } else if (height == 24) {
-    console.log("\n#########################")
-  } else {
-    height = Number(readlineSync.question("Height: "));
+for (let i = 0; i < height; i++) {
+  for (let j = 0; j < height - 1 - i; j++) {
+    console.log(" ");
   }
+  for (let k = 0; k < i + 2; k++) {
+      console.log("#");
+  }
+  console.log()
 }
